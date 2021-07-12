1e7fca8d46229f98427035beb2f524bc8dddfab9
Nimov <piotr.wieckowski@gmail.com>
2021-07-12 10:04:53 +0200

    feat(strings): added some functions with multi-line strings

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+# Multiline statements & strings section
+def string_fun_01():
+    a = '''a multi-line string
+    that is actually indented in the second line'''
+    return a
+
+
+def string_fun_02():
+    a = '''a multi-line string
+that is not indented in the second line'''
+    return a
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print(string_fun_01())
+    print(string_fun_02())
