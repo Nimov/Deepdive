e03a904ed93d688a8999d72a68793f64d095f514
Nimov <piotr.wieckowski@gmail.com>
2021-07-13 19:05:32 +0200

    feat(classes): added basic practice with classes

--- main.py
+++ main.py
@@ -181,6 +181,59 @@ def for_fun_indexing(s: str):
         print(i, c)
 
 
+# CLASSES
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        return 2 * self._height + 2 * self._width
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width <= 0:
+            raise ValueError('Width must be positive!')
+        else:
+            self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if height <= 0:
+            raise ValueError('Height must be positive!')
+        else:
+            self._height = height
+
+    def __str__(self):
+        return 'Rectangle: width={0}, height={1}'.format(self.width, self.height)
+
+    def __repr__(self):
+        return 'Rectangle({0}, {1})'.format(self.width, self.height)
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.width == other.width and self.height == other.height
+        else:
+            return False
+
+    def __lt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area() < other.area()
+        else:
+            return NotImplemented
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('--- MULTILINE STRINGS ---')
@@ -207,3 +260,9 @@ if __name__ == '__main__':
     for_fun_break_continue()
     for_fun_search()
     for_fun_indexing("Piotr")
+    print('\n--- CLASSES ---')
+    r1 = Rectangle(10, 20)
+    print(str(r1))
+    print(repr(r1))
+    r2 = Rectangle(10, 20)
+    print('{0}'.format(r1 == r2))
